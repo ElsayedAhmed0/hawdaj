72741e794b461ef4e13ad9304c0a9d6177bba147
elsayed khalil <e.khalil@evntoo.com>
2023-01-05 21:37:16 +0200

    edite card and new d

--- resources/views/front/swalef/all.blade.php
+++ resources/views/front/swalef/all.blade.php
@@ -3,36 +3,149 @@
 
 <section class="filter-grid-section filter-grid-section--height">
     <div class="container">
-    <h2 class="page-title mb-4">Obcaecati modi et nu</h2>
+        <h2 class="page-title mb-4">Obcaecati modi et nu</h2>
         <div class="section-boxes container">
 
             <!-- anim-project a1 -->
             <div class="section-boxes-wrap">
-                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+
                 <div class="section-title">
                     <div class="section-projects-overflow">
-                        <div class="section-box ">
-                            <div class="swiper-container swiper3">
-                                <div class="swiper-wrapper">
-                                    <div class="swiper-zooms swiper-slide">
-                                        <div class="swiper-zoom-container">
-                                            <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                        <div class="card-row">
+                            <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <!-- <span class="wishlist"><i class="fa-regular fa-heart"></i></span> -->
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                        <span class="date-card"> <i class="fa-solid fa-calendar-days"></i>
+                                            12-1-2023</span>
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type"><span>Design and Development</span></div>
+                                        <div class="project-title"><span>consectetur adipisicing.</span></div>
+                                        <div class="arte-type d-flex justify-content-between">
+                                            <span class="type-project"><span>lecture</span></span>
+                                            <span class="rate-project">
+                                                <em class="rating-">
+                                                    <i class="fa-regular fa-star"></i>
+                                                    <i class="fa-regular fa-star"></i>
+                                                    <i class="fa-regular fa-star"></i>
+                                                    <i class="fa-regular fa-star"></i>
+                                                    <i class="fa-regular fa-star"></i>
+
+                                                </em>
+                                            </span>
                                         </div>
                                     </div>
-                                </div>
+                                    <div class="see-more-n">
+                                        <span class="see-m-nd">
+                                            see more
+                                        </span>
+                                    </div>
+                                </a>
                             </div>
-                        </div>
-                        <div class="title-area">
-                            <div class="project-type">Design and Development</div>
-                            <div class="project-title">consectetur adipisicing.</div>
-                            <span class="type-project">lecture</span>
+                            <!-- <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type">Design and Development</div>
+                                        <div class="project-title">consectetur adipisicing.</div>
+                                        <span class="type-project">lecture</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type">Design and Development</div>
+                                        <div class="project-title">consectetur adipisicing.</div>
+                                        <span class="type-project">lecture</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type">Design and Development</div>
+                                        <div class="project-title">consectetur adipisicing.</div>
+                                        <span class="type-project">lecture</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type">Design and Development</div>
+                                        <div class="project-title">consectetur adipisicing.</div>
+                                        <span class="type-project">lecture</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type">Design and Development</div>
+                                        <div class="project-title">consectetur adipisicing.</div>
+                                        <span class="type-project">lecture</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type">Design and Development</div>
+                                        <div class="project-title">consectetur adipisicing.</div>
+                                        <span class="type-project">lecture</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type">Design and Development</div>
+                                        <div class="project-title">consectetur adipisicing.</div>
+                                        <span class="type-project">lecture</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="card-seation">
+                                <a href="{{ url(app()->getLocale() . '/swalefsonepage') }}">
+                                    <div class="swiper-zoom-container">
+                                        <img src="{{asset('front_assets/imgs/our-services.jpg')}}">
+                                    </div>
+                                    <div class="title-area">
+                                        <div class="project-type">Design and Development</div>
+                                        <div class="project-title">consectetur adipisicing.</div>
+                                        <span class="type-project">lecture</span>
+                                    </div>
+                                </a>
+                            </div> -->
                         </div>
                     </div>
                 </div>
-                </a>
             </div>
             <!-- anim-project a1 -->
- 
+
 
         </div>
     </div>
